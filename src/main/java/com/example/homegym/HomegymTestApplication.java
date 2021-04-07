99b7772b8a616e58ae2d5c0fbd40d55249662ab5
dkkahm <dkkahm@gmail.com>
2021-04-07 19:15:46 +0900

    initial commit

---
+++ src/main/java/com/example/homegym/HomegymTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.homegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomegymTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomegymTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homegym/HomegymTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homegym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomegymTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
